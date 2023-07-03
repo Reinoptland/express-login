f886ef68e7fa2c213b938c482cdf385696fbd811
Reinoptland <reinoptland@gmail.com>
2023-07-03 17:20:21 +0200

    Make signup form

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require("express");
 
 const app = express();
+app.set("view engine", "ejs");
+// accept form data
+app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log("I AM ALIVE");
+});
+
+// send a form
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+app.post("/signup", (req, res) => {
+  console.log("FORM??", req.body);
 });
 
 app.get("/", (req, res) => {
